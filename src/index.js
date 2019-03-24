60321807cac164f1d3756e923ed5c07b9bcce398
Nate <github email address>
2019-03-24 19:01:18 -0400

    create task done. delete a task and additional deliverables later

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// VARIABLES:
+const ulTag = document.querySelector('#tasks')
+const formTag = document.querySelector('#create-task-form')
+//-----
+
+// EVENT LISTENERS:
+formTag.addEventListener('submit', e => {
+  e.preventDefault()
+  const taskDescription = document.querySelector('#new-task-description')
+  const newTask = document.createElement('li')
+  newTask.innerText = taskDescription.value
+  ulTag.appendChild(newTask)
 });
+//-----
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//
+// });
